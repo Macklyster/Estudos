7149ec7966f38f66e5ce471ae13c3d336537ecc0
AlexMCoder <alexmpprog@gmail.com>
2016-07-11 16:37:57 -0300

    07 - Método Final | Exemplo

---
+++ PHP - POO/MetodosFinal.class.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Um método com a palavra FINAL, significa que ela não pode sobrescrever nas classes filhas.
+No exemplo abaixo, iremos colocar na função Testador() a palavra final, se tentarmos fazer
+essas mesma função na Classe Teste ocorrerá um erro, informando que ela não pode ser sobrescrita.
+ */
+abstract class MetodosFinal
+{
+	public $Nome;
+	public $Idade;
+
+	final function Testador()
+	{
+		echo "Isso é só um teste!";
+	}
+}
+
+/*
+As classes que estebderem de Animal, obrigatóriamente devem ter o método Alimentar();
+Iremos abaixo criar uma classe Chachorro com o método Alimentar.
+ */
+
+class Teste extends MetodosFinal
+{
+	public $Testando;
+
+}
+
+$bob = new Teste();
+$bob->Nome = "Test";
+$bob->Testando = "Testing...";
+$bob->Idade = 2;
+$bob->Testador();
+?>
