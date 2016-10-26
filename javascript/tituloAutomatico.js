576a03d18647bf908782e998c9077e66dfa1743b
AlexMCoder <alexmpprog@gmail.com>
2016-10-26 21:14:13 -0200

    código desenvolvido para a plataforma forumeiros

---
+++ javascript/tituloAutomatico.js
@@ -0,0 +1,12 @@
+/*
+Código feito por AlexMCoder
+Desenvolvido para a plataforma Forumeiros
+*/
+
+$(function(){
+	if(/f=(?:2)&mode=newtopic/.test(window.location.search)){
+		var link = $(this).attr("href");
+		var name = $(this).
+		$('#text_editor_textarea').val();
+	}
+});
